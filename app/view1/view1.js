d34d9c540f77f7de03150e955b0c2ad164d6a037
adjoseph <aarondylanjoseph@gmail.com>
2017-04-03 18:30:30 -0700

    added functionality to grab headlines from theonion in addition to nottheonion. also changed redditservice so that it takes the url as a parameter

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,16 +10,28 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', '$http', 'RedditService', function($scope, $http, RedditService) {
+	function getHeadlines (){
+		//get nottheonion (serious) headlines
+		RedditService.getHeadlines("https://www.reddit.com/r/nottheonion/new.json?sort=new&limit=10")
+		.then(function(posts){
+			for (var i = 0; i < posts.length; i++) {
+				$scope.headlines.push(posts[i].data)
+			}
+		});
+
+		//get onion (satire) headlines 
+		RedditService.getHeadlines("https://www.reddit.com/r/theonion/new.json?sort=new&limit=20")
+		.then(function(posts){
+			for (var i = 0; i < posts.length; i++) {
+				if(posts[i].data.domain == "theonion.com") //must filter out headlines not from the Onion
+					$scope.headlines.push(posts[i].data)
+			}
+		});
+	}
+
 	$scope.headlines = [];
 
-	getHeadlines()
+	getHeadlines();
+	console.log($scope.headlines)
 
-	function getHeadlines (){
-		RedditService.getHeadlines()
-		.then(function(data){
-			$scope.headlines = data;
-		});
-		//"https://www.reddit.com/r/theonion/new.json?sort=new&limit=10"
-	}
-	
 }]);
